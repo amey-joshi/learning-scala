1db5d8454c33b736e7913e3768dd604e82408bc8
Amey Joshi <amey.s.joshi@acm.org>
2019-06-12 20:33:46 +0530

    Reformatted code.

--- src/main/scala/Rational.scala
+++ src/main/scala/Rational.scala
@@ -1,10 +1,10 @@
 class Rational(n: Int, d: Int = 1) {
   require(d != 0)
-  import math.abs
-  import math.signum
 
-  val numerator: Int = signum(d) * n/gcd(abs(n), abs(d))
-  val denominator: Int = signum(d) * d/gcd(abs(n), abs(d))
+  import math.{abs, signum}
+
+  val numerator: Int = signum(d) * n / gcd(abs(n), abs(d))
+  val denominator: Int = signum(d) * d / gcd(abs(n), abs(d))
 
   private def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
 
@@ -12,10 +12,14 @@ class Rational(n: Int, d: Int = 1) {
 
   // Algebraic operators
   def unary_- = new Rational(-this.numerator, this.denominator)
+
   def +(that: Rational): Rational =
     new Rational(this.numerator * that.denominator + this.denominator * that.numerator, this.denominator * that.denominator)
+
   def -(that: Rational): Rational = this + (-that)
+
   def *(that: Rational): Rational = new Rational(this.numerator * that.numerator, this.denominator * that.denominator)
+
   def /(that: Rational): Rational =
     if (that.numerator != 0) new Rational(this.numerator * that.denominator, this.denominator * that.numerator)
     else throw new IllegalAccessException("Attempting to divide by zero.")
@@ -27,6 +31,8 @@ class Rational(n: Int, d: Int = 1) {
     this.numerator == that.asInstanceOf[Rational].numerator && this.denominator == that.asInstanceOf[Rational].denominator
 
   def <=(that: Rational): Boolean = this < that || (this equals that)
+
   def >=(that: Rational): Boolean = this > that || (this equals that)
+
   def >(that: Rational): Boolean = !(that < this && (that equals this))
 }
