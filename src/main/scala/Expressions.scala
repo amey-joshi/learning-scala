d20ec5043c493c26756e62b690ec231a4a21a638
Amey Joshi <amey.s.joshi@acm.org>
2019-07-27 16:34:21 +0530

    Implemented using case classes.

--- src/main/scala/Expressions.scala
+++ src/main/scala/Expressions.scala
@@ -1,63 +1,42 @@
 trait Expr {
-  // Methods to identify
-  def isNumber: Boolean
-  def isSum: Boolean
-  def isProduct: Boolean
+  def eval: Int = this match {
+    case Number(n) => n
+    case Sum(e1, e2) => e1.eval + e2.eval
+    case Product(e1, e2) => e1.eval * e2.eval
+  }
 
-  // Methods to access data
-  def numValue: Int
-  def leftOp: Expr
-  def rightOp: Expr
-
-  def eval: Int = {
-    if(isNumber)
-      numValue
-    else if(isSum)
-      leftOp.eval + rightOp.eval
-    else if(isProduct)
-      leftOp.eval * rightOp.eval
-    else
-      throw new Error("Parse error.")
+  def show: String = this match {
+    case Number(n) => n.toString
+    case Sum(e1, e2) => s"(${e1.show} + ${e2.show})"
+    case Product(e1, e2) => s"(${e1.show} * ${e2.show})"
   }
 }
 
-class Number(val n: Int) extends Expr {
-  def isNumber = true
-  def isSum = false
-  def isProduct = false
-
+case class Number(val n: Int) extends Expr {
   def numValue = n
   def leftOp = throw new Error("Number.leftOp")
   def rightOp = throw new Error("Number.rightOp")
 }
 
-class Sum(val e1: Expr, val e2: Expr) extends Expr {
-  def isNumber = false
-  def isSum = true
-  def isProduct = false
-
+case class Sum(val e1: Expr, val e2: Expr) extends Expr {
   def numValue = throw new Error("Sum.numValue")
   def leftOp = e1
   def rightOp = e2
 }
 
-class Product(val e1: Expr, val e2: Expr) extends Expr {
-  def isNumber = false
-  def isSum = false
-  def isProduct = true
-
+case class Product(val e1: Expr, val e2: Expr) extends Expr {
   def numValue = throw new Error("Product.numValue")
   def leftOp = e1
   def rightOp = e2
 }
 
 object ExprTest extends App {
-  val e1 = new Sum(new Number(3), new Number(4))
-  val e2 = new Sum(new Number(5), e1)
-  val e3 = new Product(e1, e2)
+  val e1 = Sum(Number(3), Number(4))
+  val e2 = Sum(Number(5), e1)
+  val e3 = Product(e1, e2)
 
-  println(s"e1 = ${e1.eval}")
-  println(s"e2 = ${e2.eval}")
-  println(s"e3 = ${e3.eval}")
+  println(s"${e1.show} = ${e1.eval}")
+  println(s"${e2.show} = ${e2.eval}")
+  println(s"${e3.show} = ${e3.eval}")
 }
 
