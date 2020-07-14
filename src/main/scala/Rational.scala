593717e8b8076da6566742715c45cb254ba7b042
Amey Joshi <ameyjoshi@localhost.localdomain>
2020-07-14 19:43:06 +0530

    Many more changes.

--- src/main/scala/Rational.scala
+++ src/main/scala/Rational.scala
@@ -1,38 +1,55 @@
 class Rational(n: Int, d: Int = 1) {
   require(d != 0)
 
-  import math.{abs, signum}
+  def this(n: Int) = this(n, 1) // Integers are rational numbers.
 
-  val numerator: Int = signum(d) * n / gcd(abs(n), abs(d))
-  val denominator: Int = signum(d) * d / gcd(abs(n), abs(d))
+  // Having a private field g saves the computation of gcd twice.
+  // The compiler does *not* optimise by calculating the common sub-
+  // expression only once.
+  private val g: Int = gcd(n.abs, d.abs)
+  val num: Int = d.sign * n / g // The numerator.
+  val den: Int = d.sign * d / g // The denominator.
 
   private def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
 
-  override def toString: String = numerator + "/" + denominator
+  override def toString: String = if (den != 1) s"$num/$den" else s"$num"
 
   // Algebraic operators
-  def unary_- = new Rational(-this.numerator, this.denominator)
+  def unary_- = new Rational(-num, den)
 
-  def +(that: Rational): Rational =
-    new Rational(this.numerator * that.denominator + this.denominator * that.numerator, this.denominator * that.denominator)
+  def +(z: Rational): Rational =
+    new Rational(num * z.den + den * z.num, den * z.den)
 
-  def -(that: Rational): Rational = this + (-that)
+  // Do not define this method as 
+  // this + (-z)
+  // It creates a new object -z that serves no purpose.
+  def -(z: Rational): Rational = 
+    new Rational(num * z.den + den * z.num, den * z.den)
 
-  def *(that: Rational): Rational = new Rational(this.numerator * that.numerator, this.denominator * that.denominator)
+  def *(z: Rational): Rational = 
+    new Rational(num * z.num, den * z.den)
 
-  def /(that: Rational): Rational =
-    if (that.numerator != 0) new Rational(this.numerator * that.denominator, this.denominator * that.numerator)
+  def /(z: Rational): Rational =
+    if (z.num != 0) new Rational(num * z.den, den * z.num)
     else throw new IllegalAccessException("Attempting to divide by zero.")
 
+  // Overloaded methods
+  def +(n: Int) = new Rational(num + n*den, den)
+  def -(n: Int) = new Rational(num - n*den, den)
+  def *(n: Int) = new Rational(n * num, den)
+  def /(n: Int) = new Rational(num, n * den)
+
   // Comparison operators
-  def <(that: Rational): Boolean = this.numerator * that.denominator < this.denominator * that.numerator
+  def <(z: Rational): Boolean = num * z.den < den * z.num
 
-  override def equals(that: Any): Boolean = that.isInstanceOf[Rational] &&
-    this.numerator == that.asInstanceOf[Rational].numerator && this.denominator == that.asInstanceOf[Rational].denominator
+  override def equals(z: Any): Boolean = 
+    z.isInstanceOf[Rational] &&
+    num == z.asInstanceOf[Rational].num && 
+    den == z.asInstanceOf[Rational].den
 
-  def <=(that: Rational): Boolean = this < that || (this equals that)
+  def <=(z: Rational): Boolean = this < z || (this equals z)
 
-  def >=(that: Rational): Boolean = this > that || (this equals that)
+  def >=(z: Rational): Boolean = this > z || (this equals z)
 
-  def >(that: Rational): Boolean = !(that < this && (that equals this))
+  def >(z: Rational): Boolean = !(z < this && (z equals this))
 }
